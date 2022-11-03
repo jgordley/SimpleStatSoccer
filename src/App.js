da37bc38a9539839b0ae45086551da5804989dc9
Jack Gordley <jgordley99@gmail.com>
2022-11-03 18:15:03 -0500

    Redirecting login to setup

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
             <LinkContainer to="/signup">
               <Nav.Link>Signup</Nav.Link>
             </LinkContainer>
-            <LinkContainer to="/login">
+            <LinkContainer to="/setup">
               <Nav.Link>Login</Nav.Link>
             </LinkContainer>
           </Nav>
